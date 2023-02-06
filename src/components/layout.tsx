6affabfbe7ac5da3f334ac19c9e2066ffad4c8b2
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-06 02:11:30 +0100

    Added Robot Font for Layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,20 @@
 import Navbar from "./navbar";
+import { Roboto } from "@next/font/google";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main className={roboto.className}>{children}</main>
     </>
   );
 }
